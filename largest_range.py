d3fd324a9174c9e8c5a2ce77e5c08a392a52275b
unknown <abdelhakim@univ-tiaret.dz>
2020-12-10 14:52:28 +0100

    first problem largest range

---
+++ largest_range.py
@@ -0,0 +1,7 @@
+#Write a function that takes in an array of integers and returns an array of length 2 representing the largest range of numbers contained in that array. 
+#The first number in the output array should be the first number in the range while the second number should be the last number in the range.
+#Arange of numbers is defined as a set of numbers that come right after each other in the set of real integers. 
+#For instance, the output array (2,6]represents the range (2,3,4,5,6), which isarange of length 5.
+#Note that numbers do not need to be ordered or adjacent in the array in order to form a range. Assume that there will only be one largest range.
+#Sample input: [1, 11,3,0,15,5,2,4,10,7,12,6] 
+#Sample output: (0,7)
